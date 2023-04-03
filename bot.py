462241d9b77cb9a376c741c15589f559e5ccdfc3
hasups <129480741+hasups@users.noreply.github.com>
2023-04-03 14:36:25 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import logging
 import os
 from flask import Flask, request
@@ -9,39 +8,32 @@ from telegram.ext import Dispatcher, MessageHandler, Filters
 openai.api_key = os.getenv("OPENAI_API_KEY")
 telegram_bot_token = str(os.getenv("TELEGRAM_BOT_TOKEN"))
 
-# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Initial Flask app
 app = Flask(__name__)
 
-# Initial bot by Telegram access token
 bot = telegram.Bot(token=telegram_bot_token)
 
+# Set route /callback with POST method will trigger this method.
 @app.route('/callback', methods=['POST'])
 def webhook_handler():
-    """Set route /callback with POST method will trigger this method."""
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
-
         # Update dispatcher process that handler to process this message
         dispatcher.process_update(update)
     return 'ok'
 
 def reply_handler(bot, update):
-    """Reply message."""
     response = openai.ChatCompletion.create(
         model ="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": update.message.text}]
+        messages=[{"role": "user", "content": update.message.text}],
+        max_tokens=256,
+        temperature=0.7
         )
     update.message.reply_text(response['choices'][0]['message']['content'].strip())
 
-# New a dispatcher for bot
 dispatcher = Dispatcher(bot, None)
-
-# Add handler for handling message, there are many kinds of message. For this handler, it particular handle text
-# message.
 dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
 
 if __name__ == "__main__":
